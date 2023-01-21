2f1ba8f583d030590090be92bd8d4e29ae891bc1
Tony <99402558+TonyReact@users.noreply.github.com>
2023-01-21 16:26:19 +0200

    Update App.js

--- tours/src/App.js
+++ tours/src/App.js
@@ -8,19 +8,35 @@ import Header from './components/Header';
 import Home from './components/Homepage';
 import SingleActivity from './components/SingleActivity';
 import './styles/global.scss'
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='activities' element={<Activities />} />
-        <Route path='activities/:activityId' element={<SingleActivity />} />
-        <Route path='about' element={<About />} />
-        <Route path='contacts' element={<Contacts />} />
-      </Routes>
-      <Footer />
+    <div className="container">
+      {loading ? (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      ) : ( 
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='activities' element={<Activities />} />
+          <Route path='activities/:activityId' element={<SingleActivity />} />
+          <Route path='about' element={<About />} />
+          <Route path='contacts' element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </div>
+      )}
     </div>
   );
 }
